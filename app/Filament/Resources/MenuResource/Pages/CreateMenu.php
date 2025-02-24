bfdafe1c1bfc11eee147a264009594cff333721f
Muhammad Alfin Pangestu <muhammadalfinpangestu@gmail.com>
2025-02-24 09:49:17 +0700

    setting up notifikasi interval 10s

--- app/Filament/Resources/MenuResource/Pages/CreateMenu.php
+++ app/Filament/Resources/MenuResource/Pages/CreateMenu.php
@@ -3,10 +3,36 @@
 namespace App\Filament\Resources\MenuResource\Pages;
 
 use App\Filament\Resources\MenuResource;
-use Filament\Actions;
+use App\Models\User;
+use Filament\Notifications\Notification;
+use Filament\Notifications\Actions\Action;
 use Filament\Resources\Pages\CreateRecord;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class CreateMenu extends CreateRecord
 {
     protected static string $resource = MenuResource::class;
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $recipient = User::where('email', 'user@gmail.com')->first();
+
+        $menu = static::getModel()::create($data);
+
+        if ($recipient) {
+            // Kirim notifikasi ke user yang ditemukan
+            Notification::make()
+                ->title('Menu berhasil dibuat')
+                ->success()
+                ->body('Menu baru telah ditambahkan.')
+                ->actions([
+                    Action::make('Lihat Menu')
+                        ->url(route('filament.admin.resources.menus.edit', ['record' => $menu->id]))
+                ])
+                ->sendToDatabase($recipient);
+        }
+
+        return $menu;
+    }
 }
--- app/Filament/Resources/MenuResource/Pages/EditMenu.php
+++ app/Filament/Resources/MenuResource/Pages/EditMenu.php
@@ -3,8 +3,12 @@
 namespace App\Filament\Resources\MenuResource\Pages;
 
 use App\Filament\Resources\MenuResource;
+use App\Models\User;
 use Filament\Actions;
+use Filament\Notifications\Actions\Action;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Database\Eloquent\Model;
 
 class EditMenu extends EditRecord
 {
@@ -15,5 +19,27 @@ class EditMenu extends EditRecord
         return [
             Actions\DeleteAction::make(),
         ];
+    }
+
+    protected function handleRecordCreation(array $data): Model
+    {
+        $recipient = User::where('email', 'user@gmail.com')->first();
+
+        $menu = static::getModel()::create($data);
+
+        if ($recipient) {
+            // Kirim notifikasi ke user yang ditemukan
+            Notification::make()
+                ->title('Menu berhasil diubah')
+                ->success()
+                ->body(`Menu $menu->nama_menu berhasi diubah.`)
+                ->actions([
+                    Action::make('Lihat Menu')
+                        ->url(route('filament.admin.resources.menus.edit', ['record' => $menu->id]))
+                ])
+                ->sendToDatabase($recipient);
+        }
+
+        return $menu;
     }
 }
